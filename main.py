e8017433c5b60c2236f281f1326633b60f7f3152
Raphael Campos <raphael01ac@gmail.com>
2023-12-14 16:26:41 -0300

    Generating mean for intervals 5 first pairs

--- main.py
+++ main.py
@@ -1,93 +1,64 @@
 import os
 import time
 import datetime
+import statistics
 
 import pandas as pd
 from dotenv import load_dotenv
 from binance.client import Client
+
+import pprint
 load_dotenv()
 
-import pprint as pprint
+INTERVALS = ['1m', '5m', '15m', '30m', '1h', '2h', '4h', '8h', '12h', '1d']
 
 api_key = os.environ.get('BINANCE_API_KEY_TEST')
 api_secret = os.environ.get('BINANCE_API_SECRET_TEST')
 
 client = Client(api_key, api_secret, testnet=False)
 
+def fetch_kline_variations(symbol, interval):
+    dicts = {}
+    klines = client.get_klines(symbol=symbol, interval=interval, limit=1)
+    kline = klines[0]
 
-def update_variation():
-    while True:
-        tickers = client.get_all_tickers()
+    old_price = float(kline[1])
+    new_price = float(kline[4])
 
-        df = pd.DataFrame(tickers)
-        current_pairs = df[df['symbol'].str.endswith("USDT")]
+    variation = ((new_price - old_price) / old_price) * 100
 
-        time.sleep(30)
-        previous_pairs = current_pairs
+    dicts[symbol] = variation
+    return dicts
 
-        tickers = client.get_all_tickers()
-        df = pd.DataFrame(tickers)
-        current_pairs = df[df['symbol'].str.endswith("USDT")]
-
-        for index, row in current_pairs.iterrows():
-            previous_pair = previous_pairs.loc[index]
-            if previous_pair['symbol'] == row['symbol']:
-                previous_price = float(previous_pair['price'])
-                current_price = float(row['price'])
-                variation = ((current_price - previous_price) / previous_price) * 100
-                print(f"pair {row['symbol']} - variation {variation}")
-
-
-def velas():
+def main():
+    start_time = time.perf_counter()
 
     tickers = client.get_all_tickers()
-
     df = pd.DataFrame(tickers)
-    current_pairs = df[df['symbol'].str.endswith("USDT")]
+    usdt_pairs  = df[df['symbol'].str.endswith("USDT")].head()
 
-    for _, row in current_pairs.iterrows():
-        candles = client.get_klines(symbol=row['symbol'], interval=Client.KLINE_INTERVAL_1MINUTE, limit=10)
-        print(f"moeda: {row['symbol']} - {candles}")
-        print("\n")
+    dictionaries = {interval: [] for interval in INTERVALS}
 
-def test_ontem():
-    tickers = client.get_all_tickers()
+    for _, row in usdt_pairs.iterrows():
+        symbol = row['symbol']
 
-    df = pd.DataFrame(tickers)
-    current_pairs = df[df['symbol'].str.endswith("USDT")]
-
-    balance = client.get_asset_balance(asset='USDT')
-    pprint.pprint(balance)
-
-    tickers = client.get_all_tickers()
-
-    df = pd.DataFrame(tickers)
-    pares = df[df['symbol'].str.endswith("USDT")]
-    price = 0
-    for _, pair in pares.iterrows():
-        if pair['symbol'] == 'BTCUSDT':
-            price = pair['price']
+        for interval in INTERVALS:
+            dictionaries[interval].append(fetch_kline_variations(symbol, interval))
 
 
-
-def convert_time(time):
-    your_dt = datetime.datetime.fromtimestamp(time / 1000)
-    return your_dt.strftime("%Y-%m-%d -- %H:%M:%S")
-
-
-while True:
-    intervals = ['1m', '5m', '15m', '30m', '1h', '2h', '4h', '8h', '12h', '1d']
-
-    for interval in intervals:
-        klines = client.get_klines(symbol='BTCUSDT', interval=interval, limit=1)
-        kline = klines[0]
-
-        old_price = float(kline[1])
-        new_price = float(kline[4])
-        open_time = kline[0]
-        close_time = kline[6]
-
-        variation = ((new_price - old_price) / old_price) * 100
-        print(f"BTCUSDT\nOpen Time: {convert_time(open_time)}\nClose time: {convert_time(close_time)}\nIntervalo: {interval}\nVariação: {variation}%\n")
+    #print test
+    print(f'Média das variações\n')
+    for interval in INTERVALS:
+        values = [value for sub_dict in dictionaries[interval] for value in sub_dict.values()]
+        mean = statistics.mean(values)
+        print(f"Média {interval}: {mean:.2f}%")
+        
+    end_time = time.perf_counter()
+    runtime = end_time - start_time
+    print(f"Tempo de execução: {runtime:.2f} segundos")
     
-    time.sleep(60)
+if __name__ == "__main__":
+    main()
+
+
+
