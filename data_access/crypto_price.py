72b84726e7f1616342acc7b066214bfa16537dac
Raphael Campos <raphaelcampos01@proton.me>
2024-04-24 16:16:11 -0300

    changing to mongodb

--- data_access/crypto_price.py
+++ data_access/crypto_price.py
@@ -1,118 +1,16 @@
-from datetime import datetime
-from datetime import timedelta
-
 import pandas as pd
-
-from utils.datetime_utils import DateTimeUtils
-from data_access.database_manager import DatabaseManager
-from utils.global_settings import GlobalSettings
-
 
 class CryptoPrice:
     """TODO: Document class"""
 
     def __init__(self, symbol: str, timestamp: int, price: float):
         """TODO: Document method"""
-
-        # Initialize attributes
         self.symbol = symbol.upper()
-        self.timestamp = timestamp
-        self.price = price
-        self.datetime = datetime.fromtimestamp(timestamp)
-        self.global_settings = GlobalSettings()
-        self.database_manager = DatabaseManager(
-            self.global_settings.CRYPTO_PRICES_DB_PATH
-        )
-
-    def _create_crypto_price_table(self, table_name: str) -> None:
-        """
-        Creates a table for storing price data of a cryptocurrency.
-
-        Args:
-            table_name (str): Name of the table to create.
-        """
-        sql = f"""
-        CREATE TABLE IF NOT EXISTS {table_name} (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            timestamp INTEGER NOT NULL,
-            price REAL NOT NULL
-        );
-        """
-        self.database_manager.execute_sql(
-            sql, f"Error creating coin table: {table_name}"
-        )
-
-    def add_to_database(self) -> None:
-        """TODO: Document method"""
-        table_name = self.database_manager.format_symbol(self.symbol)
-        if not self.database_manager.table_exists(table_name):
-            self._create_crypto_price_table(table_name)
-        sql = f"INSERT INTO {table_name} (timestamp, price) VALUES (?, ?)"
-        params = (self.timestamp, self.price)
-        self.database_manager.execute_sql(
-            sql, f"Error inserting price for {table_name}", params
-        )
-        deletion_timestamp = int(
-            (datetime.fromtimestamp(self.timestamp) - timedelta(days=1)).timestamp()
-        )
-        self.delete_from_database(table_name, deletion_timestamp)
-
-    def delete_from_database(self, table_name: str, deletion_timestamp: int) -> None:
-        """
-        Deletes price entries older than a specific timestamp for a USD symbol.
-
-        Args:
-            table_name (str): Name of the table to delete from.
-            timestamp (int): Timestamp for deletion cutoff.
-        """
-        sql = f"DELETE FROM {table_name} WHERE timestamp < ?"
-        params = (deletion_timestamp,)
-        self.database_manager.execute_sql(
-            sql, f"Error deleting price for {table_name}", params
-        )
-
-    def _get_coin_prices_dataframe(self, table_name: str) -> pd.DataFrame:
-        """
-        Retrieves price data for a USD symbol as a DataFrame.
-
-        Args:
-            table_name (str): Name of the table to fetch data from.
-
-        Returns:
-            pd.DataFrame: DataFrame containing price data.
-        """
-        table_name = self.database_manager.format_symbol(table_name)
-        if not self.database_manager.table_exists(table_name):
-            self._create_crypto_price_table(table_name)
-        sql = f"SELECT timestamp, price FROM {table_name}"
-        df = self.database_manager._execute_sql(
-            sql, f"Error selecting from {table_name}", table_name=table_name
-        )
-        if df is not None:
-            df["symbol"] = self.database_manager.format_symbol(table_name)
-        return df
-
-    def get_all_coins_dataframes(self, usdt_pairs: pd.DataFrame) -> list[pd.DataFrame]:
-        """
-        Retrieves price data for all USD symbols as a list of DataFrames.
-
-        Args:
-            usdt_pairs (pd.DataFrame): DataFrame containing USD symbol pairs.
-
-        Returns:
-            list: List of DataFrames containing price data for each symbol.
-        """
-        return [
-            self._get_coin_prices_dataframe(row["symbol"])
-            for _, row in usdt_pairs.iterrows()
-        ]
+        self.snapshot = {"timestamp": timestamp, "price": price}
 
     def __str__(self) -> str:
         """Returns a string representation of the price snapshot."""
-        return (
-            f"Symbol: {self.symbol}, Timestamp: {self.timestamp}, "
-            f"Price: {self.price:.2f}, Datetime: {self.datetime}"
-        )
+        return f"Symbol: {self.symbol}\nSnapshot: {self.snapshot}"
 
     @classmethod
     def from_series(cls, series: pd.Series, current_timestamp: int) -> "CryptoPrice":
@@ -120,19 +18,4 @@ class CryptoPrice:
         symbol = series["symbol"]
         price = series["price"]
         return cls(symbol, current_timestamp, price)
-
-    @staticmethod
-    def feed_database(crypto_prices: list["CryptoPrice"]) -> None:
-        """
-        Updates the database with price snapshots for asset pairs.
-
-        Args:
-            crypto_prices (list['CryptoPrice']): containing asset pairs and their prices.
-            current_timestamp (int): current timestamp integer
-        """
-        for crypto_price in crypto_prices:
-            crypto_price.add_to_database()
-
-        current_datetime = DateTimeUtils.get_datetime()
-        GlobalSettings.logger.info(f"USD prices updated at {current_datetime}")
 
--- data_access/database_manager.py
+++ data_access/database_manager.py
@@ -1,23 +1,121 @@
-import sqlite3
+import pymongo
+
+data_agora = {"date":new Timestamp()}
+print(data_agora)
+print(type(data_agora))
+
+# TESTES ACIMA #
+
 import pandas as pd
+import os
+from dotenv import load_dotenv
+import pymongo
+from data_access.crypto_price import CryptoPrice
 
 
 class DatabaseManager:
-    """
-    Manages data storage and retrieval from a SQLite database for asset and price information.
+    """TODO: Document class"""
 
-    Attributes:
-        db_path (str): Path to the SQLite database file.
-    """
+    def __init__(self):
+        """TODO: Document method"""
+        self.dbname = self.get_database()
 
-    def __init__(self, db_path: str):
+    def get_database(self) -> pymongo.MongoClient:
+        load_dotenv()
+        mongo_user = os.getenv("MONGO_USER")
+        mongo_password = os.getenv("MONGO_PASSWORD")
+        connection_string = f"mongodb+srv://{mongo_user}:{mongo_password}@cluster0.wovexfa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+        client = pymongo.MongoClient(connection_string)
+        return client["altadata"]
+
+    def get_collection(self, collection_name: str) -> pymongo.collection.Collection:
+        """TODO: Document method"""
+        return self.dbname[f"{collection_name}"]
+
+    def create_crypto_price(self, crypto_price: CryptoPrice) -> None:
+        """TODO: Document method"""
+        try:
+            collection_name = self.get_collection("crypto_prices")
+            document = collection_name.find_one({"symbol": self.symbol})
+            if document:
+                collection_name.update_one(
+                    {"_id": document.get("_id")},
+                    {"$push": {"snapshots": self.snapshot}},
+                )
+                self.delete_from_database()
+            else:
+                collection_name.insert_one(
+                    {"symbol": self.symbol, "snapshots": [self.snapshot]}
+                )
+        except pymongo.errors.PyMongoError as e:
+            # TODO: Change to logging
+            print(f"Error adding crypto_price to database: {e}")
+
         """
-        Initializes a DataManager object with the path to the SQLite database.
+        deletion_timestamp = int(
+            (datetime.fromtimestamp(self.timestamp) - timedelta(days=1)).timestamp()
+        )
+        self.delete_from_database(table_name, deletion_timestamp)
+        """
+
+    def delete_crypto_price(
+        self,
+        symbol: str,
+        deletion_timestamp: int,
+    ) -> None:
+        """TODO: Document method"""
+        pass
+
+    def _get_coin_prices_dataframe(self, table_name: str) -> pd.DataFrame:
+        """
+        Retrieves price data for a USD symbol as a DataFrame.
 
         Args:
-            db_path (str): Path to the SQLite database file.
+            table_name (str): Name of the table to fetch data from.
+
+        Returns:
+            pd.DataFrame: DataFrame containing price data.
         """
-        self.db_path = db_path
+        table_name = self.database_manager.format_symbol(table_name)
+        if not self.database_manager.table_exists(table_name):
+            self._create_crypto_price_table(table_name)
+        sql = f"SELECT timestamp, price FROM {table_name}"
+        df = self.database_manager._execute_sql(
+            sql, f"Error selecting from {table_name}", table_name=table_name
+        )
+        if df is not None:
+            df["symbol"] = self.database_manager.format_symbol(table_name)
+        return df
+
+    def get_all_coins_dataframes(self, usdt_pairs: pd.DataFrame) -> list[pd.DataFrame]:
+        """
+        Retrieves price data for all USD symbols as a list of DataFrames.
+
+        Args:
+            usdt_pairs (pd.DataFrame): DataFrame containing USD symbol pairs.
+
+        Returns:
+            list: List of DataFrames containing price data for each symbol.
+        """
+        return [
+            self._get_coin_prices_dataframe(row["symbol"])
+            for _, row in usdt_pairs.iterrows()
+        ]
+
+    @staticmethod
+    def feed_database(crypto_prices: list["CryptoPrice"]) -> None:
+        """
+        Updates the database with price snapshots for asset pairs.
+
+        Args:
+            crypto_prices (list['CryptoPrice']): containing asset pairs and their prices.
+            current_timestamp (int): current timestamp integer
+        """
+        for crypto_price in crypto_prices:
+            crypto_price.add_to_database()
+
+        current_datetime = DateTimeUtils.get_datetime()
+        GlobalSettings.logger.info(f"USD prices updated at {current_datetime}")
 
     @staticmethod
     def format_symbol(symbol: str) -> str:
@@ -36,108 +134,39 @@ class DatabaseManager:
             return symbol[1:]
         return symbol
 
-    def execute_sql(
-        self,
-        sql: str,
-        error_message: str,
-        params: tuple | None = None,
-        table_name: str | None = None,
-    ) -> pd.DataFrame | None:
-        """
-        Executes an SQL statement with optional parameters.
 
-        Args:
-            sql (str): The SQL statement to execute.
-            error_message (str): Error message to display in case of an exception.
-            params (tuple, optional): Parameters for the SQL statement.
-            table_name (str, optional): The name of the table to return as DataFrame.
+database = DatabaseManager()
+collection_name = database.get_collection("crypto_prices")
+serie = {"timestamp": 171335362, "price": 63122.67}
+serie1 = {"timestamp": 173334762, "price": 41223.67}
+serie2 = {"timestamp": 21545465, "price": 545422.67}
+serie3 = {"timestamp": 44564231, "price": 87422.67}
+serie4 = {"timestamp": 41245465, "price": 85422.67}
+item_3 = {"symbol": "BTCUSDT", "snapshots": [serie, serie1, serie2, serie3, serie4]}
 
-        Returns:
-            pd.DataFrame or None: DataFrame if table_name is provided, otherwise None.
-        """
-        try:
-            with sqlite3.connect(self.db_path) as conn:
-                if table_name:
-                    dataframe = (
-                        pd.read_sql(sql, conn)
-                        .sort_index(ascending=False)
-                        .reset_index(drop=True)
-                    )
-                    return dataframe
-                if params:
-                    conn.execute(sql, params)
-                else:
-                    conn.execute(sql)
-        except sqlite3.Error as e:
-            self.user_settings.logger.info(f"{error_message}: {e}")
-        return None
+# collection_name.insert_one(item_3)
 
-    def fetch_one(self, sql: str, params: tuple, error_message: str) -> tuple | None:
-        """
-        Fetches a single result from the database.
+"""
+cursor = collection_name.find({})
+for document in cursor:
+    symbol = document.get("symbol")
+    series = document.get("series", [])
+    # print(f"Symbol: {symbol}")
+    # print("Series:")
+    for serie in series:
+        timestamp = serie.get("timestamp")
+        price = serie.get("price")
+        # print(f"Timestamp: {timestamp}, Price: {price}")
+    print()  # Add a blank line between documents
 
-        Args:
-            sql (str): The SQL statement to execute.
-            params (tuple): Parameters for the SQL statement.
-            error_message (str): Error message to display in case of an exception.
-
-        Returns:
-            tuple or None: Result of the query if found, otherwise None.
-        """
-        try:
-            with sqlite3.connect(self.db_path) as conn:
-                result = conn.execute(sql, params).fetchone()
-                return result if result else None
-        except sqlite3.Error as e:
-            self.user_settings.logger.info(f"{error_message}: {e}")
-        return None
-
-    def fetch_all(self, sql: str, error_message: str) -> list[tuple] | None:
-        """
-        Fetches all results from the database.
-
-        Args:
-            sql (str): The SQL statement to execute.
-            error_message (str): Error message to display in case of an exception.
-
-        Returns:
-            list: List of tuples containing results from the query.
-        """
-        try:
-            with sqlite3.connect(self.db_path) as conn:
-                result = conn.execute(sql).fetchall()
-                return result
-        except sqlite3.Error as e:
-            self.user_settings.logger.info(f"{error_message}: {e}")
-        return None
-
-    def table_exists(self, table_name: str) -> bool:
-        """
-        Checks if a table exists in the database.
-
-        Args:
-            table_name (str): Name of the table to check.
-
-        Returns:
-            bool: True if the table exists, otherwise False.
-        """
-        table_name = self.format_symbol(table_name)
-        sql = "SELECT name FROM sqlite_master WHERE type='table' AND name=?"
-        return bool(
-            self.fetch_one(
-                sql, (table_name,), f"Error checking table existence for {table_name}"
-            )
-        )
-
-    def drop_table(self, table_name: str) -> None:
-        """
-        Drops a table from the database.
-
-        Args:
-            table_name (str): Name of the table to drop.
-        """
-        sql = f"DROP TABLE {table_name}"
-        self.execute_sql(sql, f"Error droping table {table_name}")
-
+filter_query = {"symbol": "BTCUSDT"}
+new_serie = {"timestamp": 123456789, "price": 9876.54}
+update_operation = {"$push": {"series": new_serie}}
+result = collection_name.update_many(filter_query, update_operation)
+if result.modified_count > 0:
+    print("New series added successfully!")
+else:
+    print("No document matched the filter query.")
+"""
 
 
